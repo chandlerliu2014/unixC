5a3b1dd6e18122d4a3dc60cf73f1d6abdda4b665
chandlerliu <chandler@localhost.localdomain>
2014-10-17 21:52:06 +0800

    add 4_5.c

---
+++ 4_5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(void)
+{
+	if(open("tempfile",O_RDWR) < 0)
+		printf("open error\n");
+	if(unlink("tempfile") < 0)
+		printf("unlink error\n");
+	printf("file unlinked\n");
+	sleep(15);
+	printf("done\n");
+	exit(0);
+
+}
